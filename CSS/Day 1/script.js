3cab51cb933eca531653301484ae49fcc63df2e2
Dheetchanaa <kanagarajdheetchanaa@gmail.com>
2025-01-01 14:52:48 +0530

    Assignment completed

---
+++ CSS/Day 1/script.js
@@ -0,0 +1,9 @@
+function sidebarOpen() {
+    var sidebarContent = document.getElementById("sidebarContent");
+    sidebarContent.classList.add('open');
+}
+
+function sidebarClose() {
+    var sidebarContent = document.getElementById("sidebarContent");
+    sidebarContent.classList.remove("open");
+}
